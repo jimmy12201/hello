45a2a5006e82754a7d21e871a0120700339a0b5c
TehAverageGuy <jimmy12201@gmail.com>
2016-08-26 18:25:57 -0500

    Extrem bonus-windows

--- hello/hello.cpp
+++ hello/hello.cpp
@@ -1,17 +1,30 @@
-
+// Testing.cpp : Defines the entry point for the console application.
+//
 
 #include "stdafx.h"
+#include <Windows.h>
+#include <Lmcons.h>
 #include <iostream>
+#include <ctype.h>
 #include <string>
+
 using namespace std;
 
-
-int main()
+int  main()
 {
-	string name;
-	cout << "What is your name? ";
-	getline(cin, name);
-	cout << "Hello," << name << "!\n";
-	
+	TCHAR username[UNLEN + 1];
+	DWORD size = UNLEN + 1;
+	GetUserName((TCHAR*)username, &size);
+	for (int i = 0; i <= size; i++)
+	{
+		if (isprint(username[i]) || isspace(username[i]))
+		{
+			cout << (char)(username[i]);
+		}
+		else
+		{
+			break;
+		}
+	}
 }
 
